ce344d1587c7a5474c8e0e12741940618a0984ff
tbreuss <thomasbreuss@gmx.ch>
2017-12-21 20:20:20 +0100

    Handling tags etc.

--- server/public/api.php
+++ server/public/api.php
@@ -11,7 +11,8 @@ use Phroute\Phroute\Dispatcher;
 use Phroute\Phroute\Exception\HttpRouteNotFoundException;
 use Medoo\Medoo;
 
-//sleep(1);
+sleep(1);
+
 $config = require '../config/database.php';
 $database = new Medoo($config);
 
@@ -19,81 +20,59 @@ $router = new RouteCollector();
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
-	header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Accept, Origin, Authorization");
-	header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-	exit;
+    header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Accept, Origin, Authorization");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+    exit;
 }
 
-function get_articles_where()
-{
-	$where = [];
-	
-	$q = isset($_GET['q']) ? $_GET['q'] : '';
-	if (!empty($q)) {
-		$where["AND"] = [
-			"OR" => [
-				"title[~]" => $q,
-				"abstract[~]" => $q,
-				"content[~]" => $q,
-			]
-		];
-	}
-	
-	$tags = isset($_GET['tags']) ? $_GET['tags'] : [];
-	if (!empty($tags)) {
-		#$where[] = [];
-	}
-	
-	return $where;
-}
 
 $router->get('/articles', function() use ($database) {
-	$where = [];
-	$q = isset($_GET['q']) ? $_GET['q'] : '';
-	if (!empty($q)) {
-		$where = [
-			"OR" => [
-				"title[~]" => $q,
-				"abstract[~]" => $q,
-				"content[~]" => $q,
-			]
-		];
-	}
-	
-	// Pagination
-	$totalCount = $database->count('articles', $where);
-		
-	$orders = [
-		'title' => ['title' => 'ASC'],
-		'changed' => ['modified' => 'DESC']
-	];
-	$orderKey = isset($_GET['sort']) ? $_GET['sort'] : 'title';
-	$where['ORDER'] = isset($orders[$orderKey]) ? $orders[$orderKey] : '';
-	
-	
-	$page = isset($_GET['page']) ? intval($_GET['page']) : 1;
-	if ($page < 1) {
-		$page = 1;
-	}
-	
-	$where['LIMIT'] = [($page - 1) * 20, 20];
-	
-	
+    $where = [];
+    $q = isset($_GET['q']) ? $_GET['q'] : '';
+    if (!empty($q)) {
+        $where = [
+            "OR" => [
+                "title[~]" => $q,
+                "abstract[~]" => $q,
+                "content[~]" => $q,
+            ]
+        ];
+    }
+
+    // Pagination
+    $totalCount = $database->count('articles', $where);
+
+    $orders = [
+        'title' => ['title' => 'ASC'],
+        'changed' => ['modified' => 'DESC']
+    ];
+    $orderKey = isset($_GET['sort']) ? $_GET['sort'] : 'title';
+    $where['ORDER'] = isset($orders[$orderKey]) ? $orders[$orderKey] : '';
+
+
+    $page = isset($_GET['page']) ? intval($_GET['page']) : 1;
+    if ($page < 1) {
+        $page = 1;
+    }
+
+    $where['LIMIT'] = [($page - 1) * 20, 20];
+
+
     $articles = $database->select('articles', ['id', 'title', 'abstract', 'tags'], $where);
-	
-	foreach ($articles as $i => $a) {
-		$articles[$i]['tags'] = explode(',', $a['tags']);
-	}
-	
+
+    foreach ($articles as $i => $a) {
+        $articles[$i]['tags'] = explode(',', $a['tags']);
+    }
+
     return [
-    	'articles' => $articles,
-    	'tags' => array_unique($tags),
-    	'paging' => [
-    		'itemsPerPage' => 20,
-			'totalItems' => $totalCount,
-			'currentPage' => $page,
-			'pageCount' => ceil($totalCount / 20)
-    	]
+        'articles' => $articles,
+        'tags' => array_unique($tags),
+        'paging' => [
+            'itemsPerPage' => 20,
+            'totalItems' => $totalCount,
+            'currentPage' => $page,
+            'pageCount' => ceil($totalCount / 20)
+        ]
     ];
 });
 
@@ -104,94 +83,87 @@ $router->get('/articles/{id}', function($id) use ($database) {
     }
     $database->update('articles', [	"views[+]" => 1], ['id' => $id]);
     $article = handle_custom_tags($article, 'content');
-	$article['tags'] = explode(',', $article['tags']);    
+    $article['tags'] = explode(',', $article['tags']);
     return $article;
 });
 
 $router->post('/add-article', function() use ($database) {
-	$json = file_get_contents('php://input');
-	$data = json_decode($json, true);
+    $json = file_get_contents('php://input');
+    $data = json_decode($json, true);
 
-	$errors = [];
-	if (empty($data['title'])) {
-		$errors['title'] = 'Bitte einen Titel eingeben';
-	}	
-	if (empty($data['abstract'])) {
-		$errors['abstract'] = 'Bitte einen Abstract eingeben';
-	}	
-	if (empty($data['content'])) {
-		$errors['content'] = 'Bitte einen Content eingeben';
-	}	
-	if (empty($data['tags'])) {
-		$errors['tags'] = 'Bitte Tags eingeben';
-	}
-	
-	if (empty($errors)) {
-		$data['created'] = date('Y-m-d H:i:s');
-		$data['modified'] = date('Y-m-d H:i:s');
-		$database->insert('articles', $data);
-		return ['success' => true];
-	}
-	
-	return [
-		'success' => false,
-		'errors' => $errors
-	];
+    $errors = [];
+    if (empty($data['title'])) {
+        $errors['title'] = 'Bitte einen Titel eingeben';
+    }
+    if (empty($data['abstract'])) {
+        $errors['abstract'] = 'Bitte einen Abstract eingeben';
+    }
+    if (empty($data['content'])) {
+        $errors['content'] = 'Bitte einen Content eingeben';
+    }
+    if (empty($data['tags'])) {
+        $errors['tags'] = 'Bitte Tags eingeben';
+    }
+
+    if (empty($errors)) {
+        $data['created'] = date('Y-m-d H:i:s');
+        $data['modified'] = date('Y-m-d H:i:s');
+        $database->insert('articles', $data);
+        return ['success' => true];
+    }
+
+    return [
+        'success' => false,
+        'errors' => $errors
+    ];
 });
 
 $router->get('/selectedtags', function() use ($database) {
-	$where = get_articles_where();
-	
-	$sql = "
-		SELECT tags 
-		FROM articles
-		WHERE 1=1
-	";
-	
-	$conditions = [];
-	$q = isset($_GET['q']) ? $_GET['q'] : '';
-	if (!empty($q)) {
-		$conditions[] = "title LIKE '%${q}%'";
-		$conditions[] = "abstract LIKE '%${q}%'";
-		$conditions[] = "content LIKE '%${q}%'";
-	}	
-	if (!empty($conditions)) {
-		$sql .= " AND (" . implode(' OR ', $conditions) . ")";
-	}
-	
-	$conditions = [];
-	$tags = isset($_GET['tags']) ? $_GET['tags'] : [];
-	if (!empty($tags)) {
-		#$where[] = [];
-		foreach ($tags as $tag) {
-			$conditions[] = "FIND_IN_SET('${tag}', tags) > 0";
-		}
-	}	
-	if (!empty($conditions)) {
-		$sql .= " AND " . implode(' AND ', $conditions);
-	}
 
-    $rows = $database->query($sql)->fetchAll();    
-        
-	$tags = [];    
-    foreach ($rows as $row) {
-	    $tags = array_merge($tags, explode(',', $row['tags']));
+    $q = isset($_GET['q']) ? $_GET['q'] : '';
+    $tags = isset($_GET['tags']) ? $_GET['tags'] : [];
+
+    $sql = "SELECT tags FROM articles WHERE 1=1";
+
+    $params = [];
+
+    if (!empty($q)) {
+        $sql .= " AND (title LIKE ? OR abstract LIKE ? OR content LIKE ?)";
+        $params[] = $q;
+        $params[] = $q;
+        $params[] = $q;
     }
 
-	$tags = array_unique($tags);
-	sort($tags);
-	
-	return $tags;
+    if (!empty($tags)) {
+        foreach ($tags as $tag) {
+            $sql .= " AND FIND_IN_SET(?, tags)>0";
+            $params[] = $tag;
+        }
+    }
+
+    $stmt = $database->pdo->prepare($sql);
+    $stmt->execute($params);
+    $columns = $stmt->fetchAll(PDO::FETCH_COLUMN, 'tags');
+
+    $tags = [];
+    foreach ($columns as $column) {
+        $tags = array_merge($tags, explode(',', $column));
+    }
+
+    $tags = array_unique($tags);
+    sort($tags);
+
+    return $tags;
 });
 
 $router->get('/tags', function() use ($database) {
-	$orders = [
-		'name' => ['name' => 'ASC'],
-		'frequency' => ['frequency' => 'DESC'],
-		'changed' => ['modified' => 'DESC']
-	];
-	$sort = isset($_GET['sort']) ? $_GET['sort'] : 'name';
-	$order = isset($orders[$sort]) ? $orders[$sort] : $orders['name'];
+    $orders = [
+        'name' => ['name' => 'ASC'],
+        'frequency' => ['frequency' => 'DESC'],
+        'changed' => ['modified' => 'DESC']
+    ];
+    $sort = isset($_GET['sort']) ? $_GET['sort'] : 'name';
+    $order = isset($orders[$sort]) ? $orders[$sort] : $orders['name'];
     $articles = $database->select('tags', ['id', 'name', 'frequency'], ['ORDER' => $order]);
     return $articles;
 });
@@ -202,25 +174,25 @@ $router->get('/tags/{id}', function($id) use ($database) {
 });
 
 $router->get('/popular', function() use ($database) {
-	$where = [];
-	$where['ORDER'] = ['views' => 'DESC'];
-	$where['LIMIT'] = 5;
+    $where = [];
+    $where['ORDER'] = ['views' => 'DESC'];
+    $where['LIMIT'] = 5;
     $articles = $database->select('articles', ['id', 'title', 'abstract', 'views'], $where);
     return $articles;
 });
 
 $router->get('/latest', function() use ($database) {
-	$where = [];
-	$where['ORDER'] = ['created' => 'DESC'];
-	$where['LIMIT'] = 5;
+    $where = [];
+    $where['ORDER'] = ['created' => 'DESC'];
+    $where['LIMIT'] = 5;
     $articles = $database->select('articles', ['id', 'title', 'abstract', 'created'], $where);
     return $articles;
 });
 
 $router->get('/modified', function() use ($database) {
-	$where = [];
-	$where['ORDER'] = ['modified' => 'DESC'];
-	$where['LIMIT'] = 5;
+    $where = [];
+    $where['ORDER'] = ['modified' => 'DESC'];
+    $where['LIMIT'] = 5;
     $articles = $database->select('articles', ['id', 'title', 'abstract', 'modified'], $where);
     return $articles;
 });
@@ -237,29 +209,29 @@ echo json_encode($response);
 
 function handle_custom_tags($article, $key)
 {
-	$content = $article[$key];
-	
-	$replacements = [
-		'<css>' => "``` css",
-		'</css>' => "```",	
-		'<html>' => "``` html",
-		'</html>' => "```",	
-		'<javascript>' => "``` javascript",
-		'</javascript>' => "```",	
-		'<php>' => "``` php",
-		'</php>' => "```",
-		'<shell>' => "``` shell",
-		'</shell>' => "```",
-		'<sql>' => "``` sql",
-		'</sql>' => "```",
-		'<text>' => "``` text",
-		'</text>' => "```",
-		'<typoscript>' => "``` typoscript",
-		'</typoscript>' => "```",
-	];
-		
-	$content = str_replace(array_keys($replacements), array_values($replacements), $content);
-	
-	$article[$key] = $content;
-	return $article;
+    $content = $article[$key];
+
+    $replacements = [
+        '<css>' => "``` css",
+        '</css>' => "```",
+        '<html>' => "``` html",
+        '</html>' => "```",
+        '<javascript>' => "``` javascript",
+        '</javascript>' => "```",
+        '<php>' => "``` php",
+        '</php>' => "```",
+        '<shell>' => "``` shell",
+        '</shell>' => "```",
+        '<sql>' => "``` sql",
+        '</sql>' => "```",
+        '<text>' => "``` text",
+        '</text>' => "```",
+        '<typoscript>' => "``` typoscript",
+        '</typoscript>' => "```",
+    ];
+
+    $content = str_replace(array_keys($replacements), array_values($replacements), $content);
+
+    $article[$key] = $content;
+    return $article;
 }
