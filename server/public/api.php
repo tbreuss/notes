029749a7252ff909f67d78e6579a5b198c9c9097
tbreuss <thomasbreuss@gmx.ch>
2017-12-30 08:52:13 +0100

    Added exception error handler

--- server/public/api.php
+++ server/public/api.php
@@ -10,124 +10,20 @@ use Phroute\Phroute\Dispatcher;
 use Phroute\Phroute\Exception\HttpRouteNotFoundException;
 use Phroute\Phroute\RouteCollector;
 
-#sleep(1);
-
-if (request\method() === 'OPTIONS') {
-    header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
-    header('Access-Control-Allow-Headers: X-Requested-With, Content-Type, Accept, Origin, Authorization');
-    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-    exit;
-}
-
-$router = new RouteCollector();
-
-$router->filter('auth', function(){
-    $jwt = jwt\get_bearer_token();
-    if (empty($jwt)) {
-        header('HTTP/1.0 401 Unauthorized');
-        exit;
-    }
-});
-
-$router->get('/articles', function (): array {
-
-    $q = request\get_var('q', '');
-    $tags = request\get_var('tags', []);
-    $page = max(request\get_var('page', 1), 1);
-    $order = request\get_var('sort', 'default');
-    $itemsPerPage = 20;
-
-    $articles = db\article\find_all($q, $tags, $order, $page, $itemsPerPage);
-    $totalCount = db\article\found_rows();
-    $paging = db\article\paging($totalCount, $page, $itemsPerPage);
-
-    return [
-        'articles' => $articles,
-        'paging' => $paging
-    ];
-});
-
-$router->get('/articles/{id}', function (int $id): array {
-    $article = db\article\find_one($id);
-    db\article\increase_views($id);
-    return $article;
-});
-
-$router->post('/add-article', function (): array {
-    $data = request\php_input();
-    $errors = db\article\validate($data);
-    if (empty($errors)) {
-        db\article\insert($data);
-        header('HTTP/1.0 201 Created');
-        return [];
-    }
-    header('HTTP/1.0 400 Validation failed');
-    return $errors;
-}, ['before' => 'auth']);
-
-$router->put('/articles/{id}', function (int $id): array {
-    $data = request\php_input();
-    $errors = db\article\validate($data);
-    if (empty($errors)) {
-        db\article\update($id, $data);
-        header('HTTP/1.0 201 Created');
-        return [];
-    }
-    header('HTTP/1.0 400 Validation failed');
-    return $errors;
-}, ['before' => 'auth']);
-
-$router->delete('/articles/{id}', function (int $id) {
-    db\article\delete($id);
-}, ['before' => 'auth']);
-
-$router->get('/selectedtags', function (): array {
-    $q = request\get_var('q', '');
-    $tags = request\get_var('tags', []);
-    $selected = db\tag\find_selected_tags($q, $tags);
-    return $selected;
-});
-
-$router->get('/tags', function (): array {
-    $sort = request\get_var('sort', 'name');
-    return db\tag\find_all($sort);
-});
-
-$router->get('/tags/{id}', function (int $id): array {
-    return db\tag\find_one($id);
-});
-
-$router->get('/popular', function (): array {
-    return db\article\find_selected(['id', 'title', 'abstract', 'views'], ['views' => 'DESC']);
-});
-
-$router->get('/latest', function (): array {
-    return db\article\find_selected(['id', 'title', 'abstract', 'created'], ['created' => 'DESC']);
-});
-
-$router->get('/modified', function (): array {
-    return db\article\find_selected(['id', 'title', 'abstract', 'modified'], ['modified' => 'DESC']);
-});
-
-$router->post('/auth/login', function () {
-    $data = request\php_input();
-    $errors = db\user\validate_credentials($data);
-    if (empty($errors)) {
-        $user = db\user\authenticate($data['username'], $data['password']);
-        if (empty($user)) {
-            $errors['password'] = 'Benutzername oder Passwort ungültig';
-        } else {
-            $token = jwt\generate_token($user);
-            return $token;
-        }
-    }
-    header('HTTP/1.0 400 Validation failed');
-    return $errors;
-});
-
+set_error_handler("exception_error_handler");
 
 try {
-    $dispatcher = new Dispatcher($router->getData());
+
+    #sleep(1);
+
+    if (request\method() === 'OPTIONS') {
+        header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
+        header('Access-Control-Allow-Headers: X-Requested-With, Content-Type, Accept, Origin, Authorization');
+        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+        exit;
+    }
+
+    $dispatcher = get_dispatcher();
     $response = $dispatcher->dispatch(request\method(), request\url_path());
     header('Access-Control-Allow-Origin: *');
     echo json_encode($response);
@@ -142,4 +38,130 @@ try {
     header('HTTP/1.0 500');
     echo json_encode(['error' => $e->getMessage()]);
 
+}
+
+function get_router()
+{
+    $router = new RouteCollector();
+
+    $router->filter('auth', function(){
+        $jwt = jwt\get_bearer_token();
+        if (empty($jwt)) {
+            header('HTTP/1.0 401 Unauthorized');
+            exit;
+        }
+    });
+
+    $router->get('/articles', function (): array {
+
+        $q = request\get_var('q', '');
+        $tags = request\get_var('tags', []);
+        $page = max(request\get_var('page', 1), 1);
+        $order = request\get_var('sort', 'default');
+        $itemsPerPage = 20;
+
+        $articles = db\article\find_all($q, $tags, $order, $page, $itemsPerPage);
+        $totalCount = db\article\found_rows();
+        $paging = db\article\paging($totalCount, $page, $itemsPerPage);
+
+        return [
+            'articles' => $articles,
+            'paging' => $paging
+        ];
+    });
+
+    $router->get('/articles/{id}', function (int $id): array {
+        $article = db\article\find_one($id);
+        db\article\increase_views($id);
+        return $article;
+    });
+
+    $router->post('/add-article', function (): array {
+        $data = request\php_input();
+        $errors = db\article\validate($data);
+        if (empty($errors)) {
+            db\article\insert($data);
+            header('HTTP/1.0 201 Created');
+            return [];
+        }
+        header('HTTP/1.0 400 Validation failed');
+        return $errors;
+    }, ['before' => 'auth']);
+
+    $router->put('/articles/{id}', function (int $id): array {
+        $data = request\php_input();
+        $errors = db\article\validate($data);
+        if (empty($errors)) {
+            db\article\update($id, $data);
+            header('HTTP/1.0 201 Created');
+            return [];
+        }
+        header('HTTP/1.0 400 Validation failed');
+        return $errors;
+    }, ['before' => 'auth']);
+
+    $router->delete('/articles/{id}', function (int $id) {
+        db\article\delete($id);
+    }, ['before' => 'auth']);
+
+    $router->get('/selectedtags', function (): array {
+        $q = request\get_var('q', '');
+        $tags = request\get_var('tags', []);
+        $selected = db\tag\find_selected_tags($q, $tags);
+        return $selected;
+    });
+
+    $router->get('/tags', function (): array {
+        $sort = request\get_var('sort', 'name');
+        return db\tag\find_all($sort);
+    });
+
+    $router->get('/tags/{id}', function (int $id): array {
+        return db\tag\find_one($id);
+    });
+
+    $router->get('/popular', function (): array {
+        return db\article\find_selected(['id', 'title', 'abstract', 'views'], ['views' => 'DESC']);
+    });
+
+    $router->get('/latest', function (): array {
+        return db\article\find_selected(['id', 'title', 'abstract', 'created'], ['created' => 'DESC']);
+    });
+
+    $router->get('/modified', function (): array {
+        return db\article\find_selected(['id', 'title', 'abstract', 'modified'], ['modified' => 'DESC']);
+    });
+
+    $router->post('/auth/login', function () {
+        $data = request\php_input();
+        $errors = db\user\validate_credentials($data);
+        if (empty($errors)) {
+            $user = db\user\authenticate($data['username'], $data['password']);
+            if (empty($user)) {
+                $errors['password'] = 'Benutzername oder Passwort ungültig';
+            } else {
+                $token = jwt\generate_token($user);
+                return $token;
+            }
+        }
+        header('HTTP/1.0 400 Validation failed');
+        return $errors;
+    });
+
+    return $router;
+}
+
+function get_dispatcher()
+{
+    $router = get_router();
+    $dispatcher = new Dispatcher($router->getData());
+    return $dispatcher;
+}
+
+function exception_error_handler($severity, $message, $file, $line) {
+    if (!(error_reporting() & $severity)) {
+        // Dieser Fehlercode ist nicht in error_reporting enthalten
+        return;
+    }
+    throw new ErrorException($message, 0, $severity, $file, $line);
 }
